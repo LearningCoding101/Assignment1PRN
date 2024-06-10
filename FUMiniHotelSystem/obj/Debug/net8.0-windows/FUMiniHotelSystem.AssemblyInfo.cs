2acb39fee2e71bdfad1f9aeb402f38f68d4ae984
LearningCoding101 <hahoang33322@gmail.com>
2024-06-10 17:22:50 +0700

    Created model, service, repository folder for 3 layer architecture

--- FUMiniHotelSystem/obj/Debug/net8.0-windows/FUMiniHotelSystem.AssemblyInfo.cs
+++ FUMiniHotelSystem/obj/Debug/net8.0-windows/FUMiniHotelSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FUMiniHotelSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+572242f8e34c6d840d415da97c22503467b49749")]
 [assembly: System.Reflection.AssemblyProductAttribute("FUMiniHotelSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FUMiniHotelSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
