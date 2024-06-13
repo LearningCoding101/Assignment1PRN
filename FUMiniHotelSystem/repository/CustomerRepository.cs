f882adfc58bfebb1499eb9a8bf19e00daabab6e1
Admin <tranduytai7@gmail.com>
2024-06-13 19:24:51 +0700

    first commit on repository class

---
+++ FUMiniHotelSystem/repository/CustomerRepository.cs
@@ -0,0 +1,124 @@
+﻿using FUMiniHotelSystem;
+using FUMiniHotelSystem.model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+
+public class CustomerRepository : IRepository<Customer> {
+    private readonly string _connectionString;
+
+    public CustomerRepository(string connectionString) {
+        _connectionString = connectionString;
+    }
+
+    public List<Customer> GetAll() {
+        List<Customer> customers = new List<Customer>();
+
+        string query = "SELECT CustomerID, CustomerFullname, Telephone, EmailAddress, CustomerBirthday, CustomerStatus " +
+                       "FROM Customer";
+
+        using (OdbcConnection connection = new OdbcConnection(_connectionString)) {
+            using (OdbcCommand command = new OdbcCommand(query, connection)) {
+                connection.Open();
+                using (OdbcDataReader reader = command.ExecuteReader()) {
+                    while (reader.Read()) {
+                        Customer customer = new Customer {
+                            CustomerID = reader.GetInt32(reader.GetOrdinal("CustomerID")),
+                            CustomerFullname = reader.GetString(reader.GetOrdinal("CustomerFullname")),
+                            Telephone = reader.IsDBNull(reader.GetOrdinal("Telephone")) ? null : reader.GetString(reader.GetOrdinal("Telephone")),
+                            EmailAddress = reader.GetString(reader.GetOrdinal("EmailAddress")),
+                            CustomerBirthday = reader.GetDateTime(reader.GetOrdinal("CustomerBirthday")),
+                            CustomerStatus = reader.GetInt32(reader.GetOrdinal("CustomerStatus"))
+                        };
+
+                        customers.Add(customer);
+                    }
+                }
+            }
+        }
+
+        return customers;
+    }
+
+    public Customer GetCustomerById(int customerId) {
+        Customer customer = null;
+
+        string query = "SELECT CustomerID, CustomerFullname, Telephone, EmailAddress, CustomerBirthday, CustomerStatus " +
+                       "FROM Customer WHERE CustomerID = ?";
+
+        using (OdbcConnection connection = new OdbcConnection(_connectionString)) {
+            using (OdbcCommand command = new OdbcCommand(query, connection)) {
+                command.Parameters.AddWithValue("@CustomerID", customerId);
+
+                connection.Open();
+                using (OdbcDataReader reader = command.ExecuteReader()) {
+                    if (reader.Read()) {
+                        customer = new Customer {
+                            CustomerID = reader.GetInt32(reader.GetOrdinal("CustomerID")),
+                            CustomerFullname = reader.GetString(reader.GetOrdinal("CustomerFullname")),
+                            Telephone = reader.IsDBNull(reader.GetOrdinal("Telephone")) ? null : reader.GetString(reader.GetOrdinal("Telephone")),
+                            EmailAddress = reader.GetString(reader.GetOrdinal("EmailAddress")),
+                            CustomerBirthday = reader.GetDateTime(reader.GetOrdinal("CustomerBirthday")),
+                            CustomerStatus = reader.GetInt32(reader.GetOrdinal("CustomerStatus"))
+                        };
+                    }
+                }
+            }
+        }
+
+        return customer;
+    }
+
+    public void AddCustomer(Customer customer) {
+        string query = "INSERT INTO Customer (CustomerFullname, Telephone, EmailAddress, CustomerBirthday, CustomerStatus, Password) " +
+                       "VALUES (?, ?, ?, ?, ?, ?)";
+
+        using (OdbcConnection connection = new OdbcConnection(_connectionString)) {
+            using (OdbcCommand command = new OdbcCommand(query, connection)) {
+                command.Parameters.AddWithValue("@CustomerFullname", customer.CustomerFullname);
+                command.Parameters.AddWithValue("@Telephone", customer.Telephone ?? (object) DBNull.Value);
+                command.Parameters.AddWithValue("@EmailAddress", customer.EmailAddress);
+                command.Parameters.AddWithValue("@CustomerBirthday", customer.CustomerBirthday);
+                command.Parameters.AddWithValue("@CustomerStatus", customer.CustomerStatus);
+                command.Parameters.AddWithValue("@Password", customer.Password);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void UpdateCustomer(Customer customer) {
+        string query = "UPDATE Customer SET CustomerFullname = ?, Telephone = ?, EmailAddress = ?, " +
+                       "CustomerBirthday = ?, CustomerStatus = ?, Password = ? WHERE CustomerID = ?";
+
+        using (OdbcConnection connection = new OdbcConnection(_connectionString)) {
+            using (OdbcCommand command = new OdbcCommand(query, connection)) {
+                command.Parameters.AddWithValue("@CustomerFullname", customer.CustomerFullname);
+                command.Parameters.AddWithValue("@Telephone", customer.Telephone ?? (object) DBNull.Value);
+                command.Parameters.AddWithValue("@EmailAddress", customer.EmailAddress);
+                command.Parameters.AddWithValue("@CustomerBirthday", customer.CustomerBirthday);
+                command.Parameters.AddWithValue("@CustomerStatus", customer.CustomerStatus);
+                command.Parameters.AddWithValue("@Password", customer.Password);
+                command.Parameters.AddWithValue("@CustomerID", customer.CustomerID);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void DeleteCustomer(int customerId) {
+        string query = "DELETE FROM Customer WHERE CustomerID = ?";
+
+        using (OdbcConnection connection = new OdbcConnection(_connectionString)) {
+            using (OdbcCommand command = new OdbcCommand(query, connection)) {
+                command.Parameters.AddWithValue("@CustomerID", customerId);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ FUMiniHotelSystem/repository/RoomRepository.cs
@@ -0,0 +1,124 @@
+﻿using FUMiniHotelSystem;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+
+public class RoomRepository : IRepository<Room> {
+    private readonly string _connectionString;
+
+    public RoomRepository(string connectionString) {
+        _connectionString = connectionString;
+    }
+
+    public List<Room> GetAll() {
+        List<Room> rooms = new List<Room>();
+
+        string query = "SELECT RoomID, RoomNumber, RoomDetailDescription, RoomMaxCapacity, RoomTypeID, RoomStatus, RoomPricePerDay FROM Room";
+
+        using (OdbcConnection connection = new OdbcConnection(_connectionString)) {
+            using (OdbcCommand command = new OdbcCommand(query, connection)) {
+                connection.Open();
+                using (OdbcDataReader reader = command.ExecuteReader()) {
+                    while (reader.Read()) {
+                        Room room = new Room {
+                            RoomID = reader.GetInt32(reader.GetOrdinal("RoomID")),
+                            RoomNumber = reader.GetString(reader.GetOrdinal("RoomNumber")),
+                            RoomDetailDescription = reader.GetString(reader.GetOrdinal("RoomDescription")),
+                            RoomMaxCapacity = reader.GetInt32(reader.GetOrdinal("RoomMaxCapacity")),
+                            RoomTypeID = reader.GetInt32(reader.GetOrdinal("RoomTypeID")),
+                            RoomStatus = reader.GetInt32(reader.GetOrdinal("RoomStatus")),
+                            RoomPricePerDay = reader.GetDecimal(reader.GetOrdinal("RoomPricePerDate"))
+                        };
+
+                        rooms.Add(room);
+                    }
+                }
+            }
+        }
+
+        return rooms;
+    }
+
+    public Room GetById(int roomId) {
+        Room room = null;
+
+        string query = "SELECT RoomID, RoomNumber, RoomDetailDescription, RoomMaxCapacity, RoomTypeID, RoomStatus, RoomPricePerDate " +
+                       "FROM Room WHERE RoomID = ?";
+
+        using (OdbcConnection connection = new OdbcConnection(_connectionString)) {
+            using (OdbcCommand command = new OdbcCommand(query, connection)) {
+                command.Parameters.AddWithValue("@RoomID", roomId);
+
+                connection.Open();
+                using (OdbcDataReader reader = command.ExecuteReader()) {
+                    if (reader.Read()) {
+                        room = new Room {
+                            RoomID = reader.GetInt32(reader.GetOrdinal("RoomID")),
+                            RoomNumber = reader.GetString(reader.GetOrdinal("RoomNumber")),
+                            RoomDetailDescription = reader.GetString(reader.GetOrdinal("RoomDescription")),
+                            RoomMaxCapacity = reader.GetInt32(reader.GetOrdinal("RoomMaxCapacity")),
+                            RoomTypeID = reader.GetInt32(reader.GetOrdinal("RoomTypeID")),
+                            RoomStatus = reader.GetInt32(reader.GetOrdinal("RoomStatus")),
+                            RoomPricePerDay = reader.GetDecimal(reader.GetOrdinal("RoomPricePerDate"))
+                        };
+                    }
+                }
+            }
+        }
+
+        return room;
+    }
+
+    public void Add(Room room) {
+        string query = "INSERT INTO Room (RoomNumber, RoomDescription, RoomMaxCapacity, RoomStatus, RoomPricePerDate, RoomTypeID) " +
+                       "VALUES (?, ?, ?, ?, ?, ?)";
+
+        using (OdbcConnection connection = new OdbcConnection(_connectionString)) {
+            using (OdbcCommand command = new OdbcCommand(query, connection)) {
+                command.Parameters.AddWithValue("@RoomNumber", room.RoomNumber);
+                command.Parameters.AddWithValue("@RoomDescription", room.RoomDescription);
+                command.Parameters.AddWithValue("@RoomMaxCapacity", room.RoomMaxCapacity);
+                command.Parameters.AddWithValue("@RoomStatus", room.RoomStatus);
+                command.Parameters.AddWithValue("@RoomPricePerDate", room.RoomPricePerDate);
+                command.Parameters.AddWithValue("@RoomTypeID", room.RoomTypeID);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void Update(Room room) {
+        string query = "UPDATE Room SET RoomNumber = ?, RoomDescription = ?, RoomMaxCapacity = ?, " +
+                       "RoomStatus = ?, RoomPricePerDate = ?, RoomTypeID = ? WHERE RoomID = ?";
+
+        using (OdbcConnection connection = new OdbcConnection(_connectionString)) {
+            using (OdbcCommand command = new OdbcCommand(query, connection)) {
+                command.Parameters.AddWithValue("@RoomNumber", room.RoomNumber);
+                command.Parameters.AddWithValue("@RoomDescription", room.RoomDescription);
+                command.Parameters.AddWithValue("@RoomMaxCapacity", room.RoomMaxCapacity);
+                command.Parameters.AddWithValue("@RoomStatus", room.RoomStatus);
+                command.Parameters.AddWithValue("@RoomPricePerDate", room.RoomPricePerDate);
+                command.Parameters.AddWithValue("@RoomTypeID", room.RoomTypeID);
+                command.Parameters.AddWithValue("@RoomID", room.RoomID);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void Delete(int roomId) {
+        string query = "DELETE FROM Room WHERE RoomID = ?";
+
+        using (OdbcConnection connection = new OdbcConnection(_connectionString)) {
+            using (OdbcCommand command = new OdbcCommand(query, connection)) {
+                command.Parameters.AddWithValue("@RoomID", roomId);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
