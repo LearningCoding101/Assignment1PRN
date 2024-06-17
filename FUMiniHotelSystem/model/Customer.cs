823374750bf9b5aa00ac4cd93aa38e2bb5355bc6
LearningCoding101 <hahoang33322@gmail.com>
2024-06-17 21:09:11 +0700

    Added remaining model

--- FUMiniHotelSystem/model/Customer.cs
+++ FUMiniHotelSystem/model/Customer.cs
@@ -8,53 +8,12 @@ namespace FUMiniHotelSystem.model
 {
     internal class Customer
     {
-        private long customerID;
-        private string? fullName;
-        private string? phone;
-        private string? email;
-        private DateTime birthday;
-        private bool status;
-        private string password = "";
-        public Customer()
-        {
-            
-        }
-        public Customer(long customerID,
-            string? fullName,
-            string? phone,
-            string? email,
-            DateTime birthday,
-            bool status,
-            string password)
-        {
-            this.customerID = customerID;
-            this.fullName = fullName;
-            this.phone = phone;
-            this.email = email;
-            this.birthday = birthday;
-            this.status = status;
-            this.password = password;
-        }
-        
-        public long CustomerID { get; set; }
-        public string Fullname { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
-        public DateTime Birthday { get; set; }
-        public bool Status { get; set; }
+        public int CustomerID { get; set; }
+        public string CustomerFullName { get; set; }
+        public string Telephone { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime? CustomerBirthday { get; set; }
+        public byte? CustomerStatus { get; set; }
         public string Password { get; set; }
-        public override string ToString()
-        {
-            string customerDetails = $"CustomerID: {CustomerID}, ";
-            customerDetails += $"FullName: {(Fullname ?? "N/A")}, "; 
-            customerDetails += $"Phone: {(Phone ?? "N/A")}, ";       
-            customerDetails += $"Email: {(Email ?? "N/A")}, ";      
-            customerDetails += $"Birthday: {Birthday}, ";
-            customerDetails += $"Status: {Status}, ";
-            customerDetails += $"Password: {(Password ?? "N/A")}";   
-            return customerDetails;
-        }
-
     }
-
-}
+    }
